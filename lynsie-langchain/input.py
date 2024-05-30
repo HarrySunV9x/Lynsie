3562be099b0b8e001845c9e4cb5e6249464bf186
HarrySunV9x <harrysunv9x@outlook.com>
2024-05-30 20:38:43 +0800

    结构拆分
    结构拆分

---
+++ lynsie-langchain/input.py
@@ -0,0 +1,33 @@
+from typing import Tuple, List
+from langchain_core.output_parsers import StrOutputParser
+from langchain.prompts.prompt import PromptTemplate
+from langchain.schema.runnable import RunnableMap, RunnablePassthrough
+from model import llm_model
+
+output_parser = StrOutputParser()
+
+_TEMPLATE = """鉴于以下对话和后续问题，请将后续问题重新表述为一个独立的问题
+
+历史对话:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:"""
+CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_TEMPLATE)
+
+def _format_chat_history(chat_history: List[Tuple]) -> str:
+    """Format chat history into a string."""
+    buffer = ""
+    for dialogue_turn in chat_history:
+        human = "Human: " + dialogue_turn[0]
+        ai = "Assistant: " + dialogue_turn[1]
+        buffer += "\n" + "\n".join([human, ai])
+    return buffer
+
+_inputs = RunnableMap(
+    standalone_question=RunnablePassthrough.assign(
+        chat_history=lambda x: _format_chat_history(x["chat_history"])
+    )
+                        | CONDENSE_QUESTION_PROMPT
+                        | llm_model
+                        | StrOutputParser(),
+)
